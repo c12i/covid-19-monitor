7a830f4c1803d90dd7836da2e4c247de637e6c4a
Collins Muriuki <murerwacollins@gmail.com>
2020-03-20 06:23:33 +0300

    Update footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,10 +4,14 @@ import { Footer } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
 import { FooterText } from './footer.styles';
 
-const FooterComponent = () => (
-    <Footer style={{padding: '20px'}}>
-        <FooterText>Copyright &copy; 2020</FooterText>
+const FooterComponent = () => {
+  const date = new Date()
+  const year = date.getFullYear();
+  return (
+    <Footer style={{ padding: '20px' }}>
+      <FooterText>Collins Muriuki &copy; {year}</FooterText>
     </Footer>
-);
+  )
+};
 
 export default FooterComponent;
