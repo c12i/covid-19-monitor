932fdb8e3ce243a0e543a1aa758893fb7293936b
Collins Muriuki <murerwacollins@gmail.com>
2020-03-20 12:43:27 +0300

    Clean up on all-cases component

--- src/components/all-cases/all-cases.component.jsx
+++ src/components/all-cases/all-cases.component.jsx
@@ -4,30 +4,28 @@ import { createStructuredSelector } from 'reselect';
 
 import { Panel, Divider } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
-import { AllCasesContainer, Key } from  './all-cases.styles';
+import { AllCasesContainer, Key } from './all-cases.styles';
 
 import { selectAll } from '../../redux/all/all.selectors';
 
 const AllCases = ({ all }) => {
-	const { cases, deaths, recovered, updated } = all;
-	const formatedDate = new Date(updated)
-	const date = formatedDate.toDateString();
-	return (
-		
-		<Panel bordered>
-			<AllCasesContainer>
-				<h4><Key>Cases</Key> <Divider vertical /> {cases}</h4>
-				<h4><Key>Deaths</Key> <Divider vertical /> {deaths}</h4>
-				<h4><Key>Recovered</Key> <Divider vertical /> {recovered}</h4>
-				<br/>
-				<small>Updated on {date}</small>
-			</AllCasesContainer>
-  	</Panel>
-	)
+  const { cases, deaths, recovered, updated } = all;
+  const formatedDate = new Date(updated);
+  return (
+    <Panel bordered>
+      <AllCasesContainer>
+        <h4><Key>Cases</Key> <Divider vertical /> {cases}</h4>
+        <h4><Key>Deaths</Key> <Divider vertical /> {deaths}</h4>
+        <h4><Key>Recovered</Key> <Divider vertical /> {recovered}</h4>
+        <br />
+        <small>Updated on {formatedDate.toDateString()}</small>
+      </AllCasesContainer>
+    </Panel>
+  )
 };
 
 const mapStateToProps = createStructuredSelector({
-	all: selectAll
+  all: selectAll
 });
 
 export default connect(mapStateToProps)(AllCases);
