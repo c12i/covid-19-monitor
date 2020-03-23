a536452e0f50f36f529da045915658635ec8bff1
Collins Muriuki <murerwacollins@gmail.com>
2020-03-24 00:17:08 +0300

    Extract an input styled component

--- src/components/search-form/search-form.component.jsx
+++ src/components/search-form/search-form.component.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Form, InputGroup } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
 
-import { styles, inputStyles } from './search-form.styles';
+import { styles, StyledInput } from './search-form.styles';
 
 const SearchForm = ({ name, value, handleChange }) => (
   <Form fluid>
     <InputGroup style={styles}>
-      <input placeholder='Search Country' style={inputStyles} type={'search'} name={name} onChange={(e) => handleChange(e)} value={value} />
+      <StyledInput placeholder='Search Country' type={'search'} name={name} onChange={(e) => handleChange(e)} value={value} />
     </InputGroup>
   </Form>
 );
--- src/components/search-form/search-form.styles.jsx
+++ src/components/search-form/search-form.styles.jsx
@@ -1,11 +1,18 @@
+import styled from 'styled-components';
+
 export const styles = {
   width: 300,
   margin: '0 auto'
 };
 
-export const inputStyles = {
-  width: '100%',
-  background: 'inherit',
-  border: 'none',
-  padding: '15px'
-};
+export const StyledInput = styled.input`
+  width: 100%;
+  background: inherit;
+  border: none;
+  padding: 15px;
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
