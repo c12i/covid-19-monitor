b701bccc414627fad4a44eafb4d8a61ece1c184b
Collins Muriuki <murerwacollins@gmail.com>
2020-04-02 14:53:29 +0300

    Adjust news articles preference

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,5 +1,5 @@
 export const urls = {
     ALL_CASES: 'https://corona.lmao.ninja/all',
     COUNTRIES: 'https://corona.lmao.ninja/countries',
-    NEWS: `https://newsapi.org/v2/everything?sortBy=publishedAt&q=coronavirus-kenya&apiKey=dfbf66c2e49e413f815493216fa86501`
+    NEWS: `https://newsapi.org/v2/everything?sortBy=publishedAt&q=coronavirus-africa&apiKey=dfbf66c2e49e413f815493216fa86501`
 };
