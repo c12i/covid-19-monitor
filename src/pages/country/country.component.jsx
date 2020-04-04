86370049d316d61edc7e1f04c968cf758547a49f
Collins Muriuki <murerwacollins@gmail.com>
2020-04-04 15:26:44 +0300

    Add back button

--- src/pages/country/country.component.jsx
+++ src/pages/country/country.component.jsx
@@ -4,11 +4,12 @@ import { createStructuredSelector } from 'reselect';
 
 import { Panel } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
+import { Previous } from './country.styles';
 
 import { fetchCountryAsync } from '../../redux/countries/countries.actions';
 import { selectCountry, selectIsFetching } from '../../redux/countries/countries.selectors';
 
-const CountryPage = ({ match, territory, loading, fetchCountryAsync }) => {
+const CountryPage = ({ match, history, territory, loading, fetchCountryAsync }) => {
   useEffect(() => {
     fetchCountryAsync(match.params.countryId);
   },[fetchCountryAsync, match.params.countryId]);
@@ -18,7 +19,8 @@ const CountryPage = ({ match, territory, loading, fetchCountryAsync }) => {
   return (
     <div>
       <br />
-      <Panel bordered>
+      <Previous to='/countries' style> &#x2B05; Back</Previous>
+      <Panel>
         { country }
         {
           console.log(country)
---
+++ src/pages/country/country.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Previous = styled(Link)`
+   &:hover {
+     text-decoration: none;
+   } 
+`;
