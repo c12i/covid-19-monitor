e6fb7c8ddc572a491a46119fb4a9a68de63996fe
Collins Muriuki <murerwacollins@gmail.com>
2020-05-07 14:11:54 +0300

    Add simple unittest

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import FooterComponent from './components/footer/footer.component';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<FooterComponent />);
+  const linkElement = getByText(/Collins Muriuki/i);
   expect(linkElement).toBeInTheDocument();
 });
