699131c6468cc4d73e44292abffc956001d4376b
Collins Muriuki <murerwacollins@gmail.com>
2020-07-15 20:24:03 +0300

    (fix) Remove news articles filters

--- src/utils/filter-articles.utils.js
+++ src/utils/filter-articles.utils.js
@@ -1,14 +1,7 @@
 export const filterNews = (articles) => {
   if (articles) {
     return articles
-      .filter((article) => article.title !== null)
-      .filter((article) =>
-        article.title
-          .toLowerCase()
-          .includes(
-            "coronavirus" || "covid-19" || "covid19" || "outbreak" || "pandemic"
-          )
-      );
+      .filter((article) => article.title !== null);
   }
   return [];
 };
