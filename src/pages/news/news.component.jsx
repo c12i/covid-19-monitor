7d1a4b30a5f76b7240e05e220f2f943ff0ba04c1
Collins Muriuki <murerwacollins@gmail.com>
2020-07-15 13:26:52 +0300

    (fix) Visualize newsapi error message

--- src/pages/news/news.component.jsx
+++ src/pages/news/news.component.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { Alert } from 'rsuite';
 
 import { fetchNewsAsync } from '../../redux/news/news.actions';
-import { selectNewsArticles, selectIsFetching } from '../../redux/news/news.selectors';
+import { selectNewsArticles, selectIsFetching, selectErrorMessage } from '../../redux/news/news.selectors';
 
 import { filterNews } from '../../utils/filter-articles.utils';
 import { getKey } from '../../utils/get-key.utils';
@@ -11,7 +12,7 @@ import { getKey } from '../../utils/get-key.utils';
 import ArticlePanel from '../../components/article-panel/article-panel.component';
 import PlaceholderPanels from '../../components/placeholders/placeholders.component';
 
-const NewsPage = ({ fetchNewsAsync, loading, articles }) => {
+const NewsPage = ({ fetchNewsAsync, loading, articles, errorMessage }) => {
   useEffect(() => {
     fetchNewsAsync();
   }, [fetchNewsAsync]);
@@ -19,17 +20,14 @@ const NewsPage = ({ fetchNewsAsync, loading, articles }) => {
   return (
     <div>
       <h3 style={{textAlign: 'center', margin: '20px'}}>News</h3>
-      {
-        loading ? <PlaceholderPanels rows={4} />
-        : 
-        filterNews(articles).map(({ source: { name }, title, description, url, publishedAt  }) => (
+      {loading && <PlaceholderPanels rows={4} />}
+      {errorMessage ? () => Alert.error(errorMessage.toString(), 5000)
+        :
+        filterNews(articles).map(({ source: { name }, ...otherProps  }) => (
           <ArticlePanel 
           key={getKey(name)}
           source={name} 
-          title={title} 
-          description={description} 
-          url={url} 
-          publishedAt={publishedAt} 
+          {...otherProps}
           />
         ))
       }
@@ -39,7 +37,8 @@ const NewsPage = ({ fetchNewsAsync, loading, articles }) => {
 
 const mapStateToProps = createStructuredSelector({
   articles: selectNewsArticles,
-  loading: selectIsFetching
+  loading: selectIsFetching,
+  errorMessage: selectErrorMessage
 });
 
 const mapDispatchToProps = dispatch => ({
