c634fb1a3befd70630dc992ccdb5da777b3ee6e2
Collins Muriuki <murerwacollins@gmail.com>
2020-07-29 20:04:50 +0300

    (fix) Update news-api issue

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,5 +1,5 @@
 export const urls = {
     ALL_CASES: 'https://corona.lmao.ninja/v2/all',
     COUNTRIES: 'https://corona.lmao.ninja/v2/countries/?sort=cases',
-    NEWS: `https://newsapi.org/v2/everything?sortBy=publishedAt&q=coronavirus-kenya&apiKey=dfbf66c2e49e413f815493216fa86501`
+    NEWS: `https://newsapi.org/v2/everything?sortBy=publishedAt&q=coronavirus-kenya&apiKey=80ec86b4b19148579a64f273b9f5a047`
 };
